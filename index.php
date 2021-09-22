7e864559e661df4fc0bfbf833e1659cc429794f1
Kabs009 <jkabera72@gmail.com>
2021-09-22 15:06:39 +0300

    Update index.php

--- index.php
+++ index.php
@@ -5,10 +5,11 @@ use Cowsayphp\Farm;
 
 header('Content-Type: text/plain');
 
-$text = "Set a message by adding ?message=<message here> to the URL";
+$text = "Hello World";
 if(isset($_GET['message']) && $_GET['message'] != '') {
 	$text = htmlspecialchars($_GET['message']);
 }
 
 $cow = Farm::create(\Cowsayphp\Farm\Cow::class);
 echo $cow->say($text);
+echo "This is great";
