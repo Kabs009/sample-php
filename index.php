6795c1657248ecc33f79d9f25f3ed70489c0c5e8
Kabs009 <jkabera72@gmail.com>
2021-09-24 14:26:24 +0300

    Update index.php

--- index.php
+++ index.php
@@ -1,9 +1,9 @@
 <html>
 
-<form>
-<input type = "text" name = "username">
-<input type = "text" name = "username">
-<input type = "submit" label = "Pay">
+<form action = "process.php" method = "POST">
+Enter Phone: <input type = "text" name = "phone"><br>
+Enter Amount: <input type = "text" name = "amount"><br>
+<input type = "submit" label = "Pay"><br>
 </form>
 </html>
 
