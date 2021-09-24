7b5592c23596489144d42860e1fe405e6cabf4a7
Kabs009 <jkabera72@gmail.com>
2021-09-25 00:00:17 +0300

    Update index.php

--- index.php
+++ index.php
@@ -7,7 +7,7 @@
     <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
   </div>
   <div class="form-group">
-    <label for="amount">Password</label>
+    <label for="amount">Amount</label>
     <input type="text" class="form-control" id="exampleInputPassword1" placeholder="Enter Amount">
   </div>
   <div class="form-check">
