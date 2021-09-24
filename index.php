7800167c8479e33a3da26766d51981fff19ef68a
Kabs009 <jkabera72@gmail.com>
2021-09-24 22:55:24 +0300

    Update index.php

--- index.php
+++ index.php
@@ -1,10 +1,11 @@
 
 <html>
-  <form action="https://sample-php-bjy54.ondigitalocean.app/transact">
+  <form action="https://sample-php-bjy54.ondigitalocean.app/transact" method = "POST">
   <label for="phone">Enter Phone:</label><br>
   <input type="text" id="Phone" name="phone" value=""><br>
   <label for="amount">Enter Amount:</label><br>
   <input type="text" id="amount" name="amount" value=""><br><br>
   <input type="submit" value="Submit">
+
 
 
