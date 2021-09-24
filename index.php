b88468818acb2f6e66ed97feba3a42be5dbba7d7
Kabs009 <jkabera72@gmail.com>
2021-09-24 14:03:14 +0300

    Update index.php

--- index.php
+++ index.php
@@ -1,14 +1,2 @@
 <?php
-require __DIR__ . '/vendor/autoload.php';
-
-use Cowsayphp\Farm;
-
-header('Content-Type: text/plain');
-
-$text = "Hello World";
-if(isset($_GET['message']) && $_GET['message'] != '') {
-	$text = htmlspecialchars($_GET['message']);
-}
-
-;
 echo "This is great";
