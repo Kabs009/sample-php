11e784af102f48ed5e9506e49db9109193f5099d
Kabs009 <jkabera72@gmail.com>
2021-09-24 13:54:35 +0300

    Update index.php

--- index.php
+++ index.php
@@ -10,6 +10,5 @@ if(isset($_GET['message']) && $_GET['message'] != '') {
 	$text = htmlspecialchars($_GET['message']);
 }
 
-$cow = Farm::create(\Cowsayphp\Farm\Cow::class);
-echo $cow->say($text);
+;
 echo "This is great";
