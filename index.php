3a33d21975024ac5e9f628604147b202f7d033f9
Kabs009 <jkabera72@gmail.com>
2021-09-25 00:09:06 +0300

    Update index.php

--- index.php
+++ index.php
@@ -1,20 +1,15 @@
 
 <html>
 <form action="https://sample-php-bjy54.ondigitalocean.app/transact" method="post">
-  <div class="form-group">
-    <label for="phone">Phone</label>
-    <input type="text" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter Phone">
-    <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
-  </div>
-  <div class="form-group">
+   
+  <input type="text" name ="phone" placeholder="Enter Phone">
+    
+  
     <label for="amount">Amount</label>
-    <input type="text" class="form-control" id="exampleInputPassword1" placeholder="Enter Amount">
-  </div>
-  <div class="form-check">
-    <input type="checkbox" class="form-check-input" id="exampleCheck1">
-    <label class="form-check-label" for="exampleCheck1">Check me out</label>
-  </div>
-  <button type="submit" class="btn btn-primary">Submit</button>
+    <input type="text" name="amount" placeholder="Enter Amount">
+  <input type="submit" value="PAY NOW">
+
+  
 </form>
 </html>
 
