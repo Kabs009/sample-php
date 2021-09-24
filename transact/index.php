053c867320c99d0189ad2cc632b3ae6a93f5c557
Kabs009 <jkabera72@gmail.com>
2021-09-24 18:58:00 +0300

    Update index.php

--- transact/index.php
+++ transact/index.php
@@ -1,6 +1,31 @@
 <?php
-  $phone = $_POST['phone'];
-  $amount = $_POST['amount'];
- echo "You have received . $amount from . $phone";
+$amount = $_POST["amount"]; //Amount to transact 
+$phonenuber = $_POST["phone"]; // Phone number paying
+$Account_no = 'ABUNDANT LIFE FAMILY CHAPEL'; // Enter account number optional
+$url = 'https://tinypesa.com/api/v1/express/initialize';
+$data = array(
+    'amount' => $amount,
+    'msisdn' => $phonenuber,
+    'account_no'=>$Account_no
+);
+$headers = array(
+    'Content-Type: application/x-www-form-urlencoded',
+    'ApiKey: t1fsmXJ53Lk' // Replace with your api key
+ );
+$info = http_build_query($data);
+
+$curl = curl_init();
+curl_setopt($curl, CURLOPT_URL, $url);
+curl_setopt($curl, CURLOPT_POST, true);
+curl_setopt($curl, CURLOPT_POSTFIELDS, $info);
+curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+curl_setopt($curl, CURLOPT_HTTPHEADER, $headers);
+$resp = curl_exec($curl);
+$msg_resp = json_decode($resp);
+
+if($msg_resp ->success == 'true'){
+    echo "PLEASE ENTER PIN";
+}
 
 ?>
+
