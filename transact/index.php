7c0491a7a0577a66d3da107563f9f6953254cdc4
Kabs009 <jkabera72@gmail.com>
2021-09-25 13:16:38 +0300

    Update index.php

--- transact/index.php
+++ transact/index.php
@@ -1,9 +1,9 @@
 <?php 
-
+if (isset($_POST['username']) || isset($_POST['email'])){
 $amount = $_POST['amount']; //Amount to transact 
 $phonenuber = $_POST['phone']; // Phone number paying
 $payacc  = 'ALFC';// Phone number paying
- 
+}
  echo "These are the variables. The amount is" . $amount ."and phone is" . $phone ."while the account is"  . $payacc;
 
 ?>
