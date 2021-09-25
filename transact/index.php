a86241ff683372b74fdc7b5c94f5b20002091943
Kabs009 <jkabera72@gmail.com>
2021-09-25 13:56:47 +0300

    Update index.php

--- transact/index.php
+++ transact/index.php
@@ -1,14 +1,11 @@
 <?php 
-if (isset($_POST['amount']) || isset($_POST['phone'])){
-$amount = $_POST['amount']; //Amount to transact 
-$phonenuber = $_POST['phone']; // Phone number paying
+$amount = $_GET['amount']; //Amount to transact 
+$phonenuber = $_GET['phone']; // Phone number paying
 $payacc  = 'ALFC';// Phone number paying
  
   echo "These are the variables. The amount is" . $amount ."and phone is" . $phone ."while the account is"  . $payacc;
-} 
-else{
- echo "There are no variables passed!";
-}
+
+
 
 
 ?>
