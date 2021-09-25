1bb60efb3cb8e57c709ce32da3989480b0ea2fad
Kabs009 <jkabera72@gmail.com>
2021-09-25 12:01:40 +0300

    Update index.php

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 
 <html>
-<form action="https://sample-php-bjy54.ondigitalocean.app/transact" method="post">
+<form action="https://sample-php-bjy54.ondigitalocean.app/transact" method="POST">
    
   <input type="text" name ="phone" placeholder="Enter Phone">
     
