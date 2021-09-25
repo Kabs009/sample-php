c4e4784b06644dd7d79dd4c9ddbb53f1154d0f28
Kabs009 <jkabera72@gmail.com>
2021-09-25 13:00:03 +0300

    Update index.php

--- index.php
+++ index.php
@@ -1,4 +1,11 @@
-
+!DOCTYPE HTML>
+<!-- THE DOCTYPE tells the browser what type of webpage to render, the doctype used in this website is HTML5 which is the most recent HTML. -->
+<html lang="en-US">
+<head>
+	<meta charset="UTF-8">
+	<title>My Pimpin Website</title>
+	<!-- THE TITLE element is the name of the page, it is also visible in the browser tab. Its always good practice to give each page an appropriate title. -->
+</head>
 <html>
 <form action="https://sample-php-bjy54.ondigitalocean.app/transact" method="POST">
    <input type="text" name ="phone" placeholder="Enter Phone"><br/>
