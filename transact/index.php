f124c2d94224f4b5bbcab0990fb456699664ea86
Kabs009 <jkabera72@gmail.com>
2021-09-25 11:55:30 +0300

    Update index.php

--- transact/index.php
+++ transact/index.php
@@ -1,9 +1,9 @@
 <?php 
-if(isset($_POST['submit'])) {
+
 $amount = $_POST['amount']; //Amount to transact 
 $phonenuber = $_POST['phone']; // Phone number paying
 $payacc  = 'ALFC';// Phone number paying
-}
+ 
  echo $amount." ". $phonenuber." ". $payacc;
 
 ?>
