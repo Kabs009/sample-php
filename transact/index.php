b7900c4b3f99ed38b3b7f2beaa1b3a7b5441beca
Kabs009 <jkabera72@gmail.com>
2021-09-25 12:37:48 +0300

    Update index.php

--- transact/index.php
+++ transact/index.php
@@ -4,6 +4,6 @@ $amount = $_POST['amount']; //Amount to transact
 $phonenuber = $_POST['phone']; // Phone number paying
 $payacc  = 'ALFC';// Phone number paying
  
- echo . $amount" . $phone"   . $payacc;
+ echo "These are the variables. The amount is" . $amount ."and phone is" . $phone ."while the account is"  . $payacc;
 
 ?>
